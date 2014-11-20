fb310d576b43cf573a8278ebd436e677c3cf941e
Damian Mee <damian.wasilewski@gmail.com>
2014-11-20 02:33:25 +0100

    Gulpfile unretarded

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,5 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var coffee = require('gulp-coffee');
-var stylus = require('gulp-stylus');
-var jade = require('gulp-jade');
+// Note the new way of requesting CoffeeScript since 1.7.x
+require('coffee-script/register');
 
-gulp.task('coffee', function() {
-	gulp.src('./src/js/*.coffee')
-		.pipe(coffee({bare: true}).on('error', gutil.log))
-		.pipe(gulp.dest('./public/js/'));
-});
-gulp.task('one', function () {
-	gulp.src('./src/css/main.styl')
-		.pipe(stylus())
-		.pipe(gulp.dest('./public/css/'));
-});
-gulp.task('templates', function() {
-  gulp.src('./src/*.jade')
-    .pipe(jade())
-    .pipe(gulp.dest('./public/'))
-});
-gulp.task('default', function() {
-	gulp.watch('./src/js/*.coffee', ['coffee']);
-	gulp.watch('./src/css/main.styl', ['one']);
-	gulp.watch('./src/*.jade', ['templates']);
-});
+// This bootstraps your Gulp's main file
+require('./Gulpfile.coffee');
